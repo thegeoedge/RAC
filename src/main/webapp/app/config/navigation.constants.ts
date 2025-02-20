0843c5a8e155bad7395a39973a374d4980cafc8e
it21273094 <it21273094@my.sliit.lk>
2025-02-20 13:11:05 +0530

    conflict fix

--- src/main/webapp/app/config/navigation.constants.ts
+++ src/main/webapp/app/config/navigation.constants.ts
@@ -1,5 +1,5 @@
-export const ASC = 'asc';
-export const DESC = 'desc';
+export const ASC = 'desc';
+export const DESC = 'asc';
 export const SORT = 'sort';
 export const ITEM_DELETED_EVENT = 'deleted';
 export const DEFAULT_SORT_DATA = 'defaultSort';
